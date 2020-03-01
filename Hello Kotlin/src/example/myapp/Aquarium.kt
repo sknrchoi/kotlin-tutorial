b041440026c457522098ef86ed0ab819b376f6ed
sknrchoi <seulkinara@gmail.com>
2020-03-01 23:52:03 +0900

    Kotlin Bootcamp for Programmers Tutorias - subclasses and ingeritance

--- Hello Kotlin/src/example/myapp/Aquarium.kt
+++ Hello Kotlin/src/example/myapp/Aquarium.kt
@@ -1,15 +1,21 @@
 package example.myapp
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+import kotlin.math.PI
+
+open class Aquarium(open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
     // Kotlin also creates the getters and setters automatically.
     /*var width: Int = 20
     var height: Int = 40
     var length: Int = 100*/
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000
         set(value) {
             height = (value * 1000) / (width * length)
         }
+
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
 
     init {
         println("aquarium initializing")
@@ -25,10 +31,21 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
     }
 
     fun printSize() {
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
 
-        println("Volume: $volume l")
+        println("Volume: $volume l Water: $water l (${water/volume*100.0}% full)")
     }
+}
+
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+        }
+    override var water = volume * 0.8
+    override val shape = "cylinder"
 }
--- Hello Kotlin/src/example/myapp/main.kt
+++ Hello Kotlin/src/example/myapp/main.kt
@@ -18,6 +18,14 @@ fun buildAquarium() {
     aquarium7.printSize()
     aquarium7.volume = 70
     aquarium7.printSize()
+
+    println("-------subclasses and inheritance-------")
+    val aquarium8 = Aquarium(length = 25, width = 25, height = 40)
+    aquarium8.printSize()
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
 
 fun main() {
