8966f4efabab66b9742f2add053864dd21755216
sknrchoi <seulkinara@gmail.com>
2020-03-03 00:04:41 +0900

    Kotlin Bootcamp for Programmers Tutorials - Use interface delegation

--- Hello Kotlin/src/example/myapp/AquariumFish.kt
+++ Hello Kotlin/src/example/myapp/AquariumFish.kt
@@ -1,8 +1,7 @@
 package example.myapp
 
-abstract class AquariumFish: FishAction {
-    abstract val color: String
-    override fun eat() = println("yum")
+interface FishColor {
+    val color: String
 }
 
 interface FishAction {
@@ -19,17 +18,24 @@ interface AquariumAction {
     }
 }
 
-class Shark : AquariumFish(), FishAction {
+class Shark
+    : FishAction by PrintingFishAction("hunt and eat fish"),
+      FishColor {
     override val color = "gray"
-    override fun eat() {
-        println("hunt and eat fish")
-    }
 }
 
-class Plecostomus : AquariumFish(), FishAction {
+class Plecostomus(fishColor: FishColor = GoldColor)
+    : FishColor by fishColor,
+      FishAction by PrintingFishAction("eat algae") {
+
+}
+
+object GoldColor : FishColor {
     override val color = "gold"
-    override fun eat() {
-        println("eat algae")
-    }
 }
 
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
